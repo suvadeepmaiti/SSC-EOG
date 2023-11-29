b1bd5fc45197e441138c6c41a2cb5604af840bb3
Suvadeep Maiti <89101512+suvadeepmaiti@users.noreply.github.com>
2023-11-30 00:27:59 +0530

    Update generate_shhs.py

--- preprocessing/shhs/generate_shhs.py
+++ preprocessing/shhs/generate_shhs.py
@@ -6,9 +6,9 @@
 
 """
 
-__author__ = "Likith Reddy, Vamsi Kumar"
+__author__ = "Suvadeep Maiti, Shivam Kumar Sharma"
 __version__ = "1.0.0"
-__email__ = "likith012@gmail.com, vamsi81523@gmail.com"
+__email__ = "suvadeep.maiti@research.iiit.ac.in"
 
 import numpy as np
 import torch
